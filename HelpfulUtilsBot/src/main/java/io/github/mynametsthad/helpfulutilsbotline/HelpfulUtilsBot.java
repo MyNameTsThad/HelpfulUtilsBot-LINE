fb027117dc5ba2d37f5aac02c22ba26884b2c1df
MyNameTsThad <pvpthadgaming@gmail.com>
2021-12-17 09:07:44 +0700

    Test4

--- HelpfulUtilsBot/src/main/java/io/github/mynametsthad/helpfulutilsbotline/HelpfulUtilsBot.java
+++ HelpfulUtilsBot/src/main/java/io/github/mynametsthad/helpfulutilsbotline/HelpfulUtilsBot.java
@@ -34,13 +34,15 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 public class HelpfulUtilsBot {
     private final Logger log = LoggerFactory.getLogger(HelpfulUtilsBot.class);
 
-    public final int verID = 1;
-    public final String verString = "0.1.0-alpha";
-    public final String packageName = "io.github.mynametsthad.helpfulutilsbotline";
+    public static final int verID = 1;
+    public static final String verString = "0.1.0-alpha";
+    public static final String packageName = "io.github.mynametsthad.helpfulutilsbotline";
 
-    public final String ChannelID = "1656718563";
-    public final String ChannelAccessToken = "QgMnFxnTQDaCG0p3a0QduN0IA3kDU1Sk6NXCd6u4XpZZYI+6UwxG02L+2NU8a/9HfV4Fv/ZXRz/jSRvMBdNm9oG61Isa1dFBiqN9aUChDZJ1oGWzTB588lhgwlaZ9M6A/IPT9BL5MNW26RGVWDT1ZQdB04t89/1O/w1cDnyilFU=";
-    public final String ChannelSecret = "0bf3be2ae818c27aa2caec62c1592332";
+    public static final String ChannelID = "1656718563";
+    public static final String ChannelAccessToken = "QgMnFxnTQDaCG0p3a0QduN0IA3kDU1Sk6NXCd6u4XpZZYI+6UwxG02L+2NU8a/9HfV4Fv/ZXRz/jSRvMBdNm9oG61Isa1dFBiqN9aUChDZJ1oGWzTB588lhgwlaZ9M6A/IPT9BL5MNW26RGVWDT1ZQdB04t89/1O/w1cDnyilFU=";
+    public static final String ChannelSecret = "0bf3be2ae818c27aa2caec62c1592332";
+
+    public char prefix = '>';
 
     public static void main(String[] args) {
         SpringApplication.run(HelpfulUtilsBot.class, args);
@@ -48,13 +50,22 @@ public class HelpfulUtilsBot {
 
     @EventMapping
     public Message handleCommandMessageEvent(MessageEvent<TextMessageContent> event) {
-        if (event.getMessage().getText().startsWith(">")){
+        if (event.getMessage().getText().startsWith(prefix)){
             TextMessage returnMessage = new TextMessage("'" + event.getMessage().getText().substring(1) + "': Invalid command and/or syntax!");
             String rawCommand = event.getMessage().getText().substring(1);
             String[] args = rawCommand.split(" ");
             if (args[0].equalsIgnoreCase("ver")){
-                returnMessage = new TextMessage("HelpfulUtilsBot-LINE version " + verString + " (" + packageName + ":" + verString + "versionID: " + verID + ") \n" +
-                                                "by IWant2TryHard (https://github.com/MyNameTsThad/HelpfulUtilsBot-LINE)");
+                returnMessage = new TextMessage("HelpfulUtilsBot-LINE version " + verString +
+                                                "\n(" + packageName + ":" + verString + " versionID: " + verID + ")" +
+                                                "\nby IWant2TryHard (https://github.com/MyNameTsThad/HelpfulUtilsBot-LINE)");
+            } else if (args[0].equalsIgnoreCase("prefix")) {
+                if (args.length > 1){
+                    prefix = args[1].charAt(0);
+                    returnMessage = new TextMessage("Prefix set to '" + prefix + "'" +
+                                                    "\n(Warning: Setting the prefix to a letter may cause unexpected bot responses in some messages.");
+                }else{
+                    returnMessage = new TextMessage("No prefix found! Please specify a prefix.");
+                }
             }
             return returnMessage;
         }
