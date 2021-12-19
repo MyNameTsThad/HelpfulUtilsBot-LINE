de85f4268372e685b64f9f18cf2c0336f56bf5d5
MyNameTsThad <pvpthadgaming@gmail.com>
2021-12-19 13:57:06 +0700

    Test8

--- HelpfulUtilsBot/src/main/java/io/github/mynametsthad/helpfulutilsbotline/HelpfulUtilsBot.java
+++ HelpfulUtilsBot/src/main/java/io/github/mynametsthad/helpfulutilsbotline/HelpfulUtilsBot.java
@@ -29,7 +29,7 @@ public class HelpfulUtilsBot {
     private final Logger log = LoggerFactory.getLogger(HelpfulUtilsBot.class);
 
     public static final int verID = 2;
-    public static final String verString = "0.1.2-alpha";
+    public static final String verString = "0.1.3-alpha";
     public static final String packageName = "io.github.mynametsthad.helpfulutilsbotline";
 
     public static final String ChannelID = "1656718563";
@@ -49,46 +49,46 @@ public class HelpfulUtilsBot {
 
     @EventMapping
     public Message handleCommandMessageEvent(MessageEvent<TextMessageContent> event) {
-        if (event.getMessage().getText().startsWith(String.valueOf(prefix))){
+        if (event.getMessage().getText().startsWith(String.valueOf(prefix))) {
             TextMessage returnMessage = new TextMessage("'" + event.getMessage().getText().substring(1) + "': Invalid command(s) and/or syntax!");
             String rawCommand = event.getMessage().getText().substring(1);
             String[] args = rawCommand.split(" ");
-            if (args[0].equalsIgnoreCase("ver")){
+            if (args[0].equalsIgnoreCase("ver")) {
                 returnMessage = new TextMessage("HelpfulUtilsBot-LINE version " + verString +
-                                                "\n(" + packageName + ":" + verString + " versionID: " + verID + ")" +
-                                                "\nby IWant2TryHard (https://github.com/MyNameTsThad/HelpfulUtilsBot-LINE)");
+                        "\n(" + packageName + ":" + verString + " versionID: " + verID + ")" +
+                        "\nby IWant2TryHard (https://github.com/MyNameTsThad/HelpfulUtilsBot-LINE)");
             } else if (args[0].equalsIgnoreCase("prefix")) {
-                if (args.length > 1){
+                if (args.length > 1) {
                     prefix = args[1].charAt(0);
                     returnMessage = new TextMessage("Prefix set to '" + prefix + "'" +
-                                                    "\n(Warning: Setting the prefix to a letter may cause unexpected bot responses in some messages.)");
-                }else{
+                            "\n(Warning: Setting the prefix to a letter may cause unexpected bot responses in some messages.)");
+                } else {
                     returnMessage = new TextMessage("No prefix found! Please specify a prefix.");
                 }
             } else if (args[0].equalsIgnoreCase("list") | args[0].equalsIgnoreCase("l")) {
-                if (args.length > 1){
+                if (args.length > 1) {
                     if (args[1].equalsIgnoreCase("new") | args[1].equalsIgnoreCase("n")) {
-                        if (args.length > 2){
+                        if (args.length > 2) {
                             StringBuilder newListName = new StringBuilder();
                             for (int i = 0; i < args.length; i++) {
-                                if (i >= 2){
+                                if (i >= 2) {
                                     newListName.append(args[i]).append(" ");
                                 }
                             }
                             ShoppingList list = new ShoppingList(newListName.toString().trim(), Collections.singletonList(event.getSource().getSenderId()));
                             lists.add(list);
                             returnMessage = new TextMessage("Added shopping list: " + list.name);
-                        }else{
+                        } else {
                             returnMessage = new TextMessage("Specify a Name!");
                         }
                     } else if (args[1].equalsIgnoreCase("view") | args[1].equalsIgnoreCase("v")) {
-                        if (args.length > 2){
-                            if (lists.isEmpty()){
+                        if (args.length > 2) {
+                            if (lists.isEmpty()) {
                                 returnMessage = new TextMessage("There are no Shopping lists!");
-                            }else{
+                            } else {
                                 try {
                                     int index = Integer.parseInt(args[2]);
-                                    if (index <= lists.size()){
+                                    if (index <= lists.size()) {
                                         ShoppingList list = lists.get(index - 1);
 
                                         //time calculation
@@ -104,10 +104,10 @@ public class HelpfulUtilsBot {
                                         StringBuilder listsMessage = new StringBuilder("Shopping List '" + list.name + "':");
                                         for (int i = 0; i < list.elements.size(); i++) {
                                             ShoppingListElement element = list.elements.get(i);
-                                            if (element.crossed){
-                                                listsMessage.append("\n").append(i + 1).append(". ~").append(list.name).append("~");
-                                            }else{
-                                                listsMessage.append("\n").append(i + 1).append(". ").append(list.name);
+                                            if (element.crossed) {
+                                                listsMessage.append("\n").append(i + 1).append(". ~").append(element.name).append("~");
+                                            } else {
+                                                listsMessage.append("\n").append(i + 1).append(". ").append(element.name);
                                             }
                                         }
                                         listsMessage.append("\n").append("Created ")
@@ -120,14 +120,14 @@ public class HelpfulUtilsBot {
                                                 .append(secondsAgo > 0 ? (secondsAgo > 1 ? (secondsAgo + " seconds ") : (secondsAgo + " second ")) : "")
                                                 .append("ago");
                                         returnMessage = new TextMessage(listsMessage.toString());
-                                    }else{
+                                    } else {
                                         returnMessage = new TextMessage(args[2] + " is outside the Shopping List's index range!");
                                     }
-                                }catch (NumberFormatException e){
+                                } catch (NumberFormatException e) {
                                     returnMessage = new TextMessage(args[2] + " is Not a Number!");
                                 }
                             }
-                        }else{
+                        } else {
                             StringBuilder message2 = new StringBuilder("Shopping Lists:");
                             for (int i = 0; i < lists.size(); i++) {
                                 ShoppingList list = lists.get(i);
@@ -136,71 +136,85 @@ public class HelpfulUtilsBot {
                             returnMessage = new TextMessage(message2.toString());
                         }
                     } else if (args[1].equalsIgnoreCase("edit") | args[1].equalsIgnoreCase("e")) {
-                        if (args.length > 2){
+                        if (args.length > 2) {
                             try {
                                 int index = Integer.parseInt(args[2]);
-                                if (index <= lists.size()){
+                                if (index <= lists.size()) {
                                     ShoppingList list = lists.get(index - 1);
-                                    if (args.length > 3){
-                                        if (args.length > 4){
-                                            if (args[3].equalsIgnoreCase("add") | args[3].equalsIgnoreCase("a")){
+                                    if (args.length > 3) {
+                                        if (args.length > 4) {
+                                            if (args[3].equalsIgnoreCase("add") | args[3].equalsIgnoreCase("a")) {
                                                 StringBuilder newItemName = new StringBuilder();
+                                                boolean isStop = false;
+                                                int countIndex = -1;
                                                 for (int i = 0; i < args.length; i++) {
-                                                    if (i >= 4){
-                                                        newItemName.append(args[i]).append(" ");
+                                                    if (i >= 4 && !isStop) {
+                                                        if (args[i].startsWith("x") | args[i].startsWith("X")) {
+                                                            try {
+                                                                int count = Integer.parseInt(args[i].substring(1));
+                                                                isStop = true;
+                                                                countIndex = i;
+                                                            } catch (NumberFormatException e) {
+                                                                newItemName.append(args[i]).append(" ");
+                                                            }
+                                                        } else {
+                                                            newItemName.append(args[i]).append(" ");
+                                                        }
                                                     }
                                                 }
-                                                if (args.length > 5){
-                                                    try {
-                                                        int count = Integer.parseInt(args[5]);
-                                                        list.AddElements(new ShoppingListElement(newItemName.toString().trim(), count));
-                                                        returnMessage = new TextMessage("Added item '" + count + " of " + newItemName.toString().trim() + "' to Shopping List '" + list.name + "'");
-                                                    }catch (NumberFormatException e){
-                                                        list.AddElements(new ShoppingListElement(newItemName.toString().trim(), 1));
-                                                        returnMessage = new TextMessage(args[5] + " is Not a Number! Using default quantity of 1." +
-                                                                "\nAdded item '1 of " + newItemName.toString().trim() + "' to Shopping List '" + list.name + "'");
-                                                    }
-                                                }else{
+                                                if (countIndex != -1) {
+                                                    int count = Integer.parseInt(args[countIndex]);
+                                                    list.AddElements(new ShoppingListElement(newItemName.toString().trim(), count));
+                                                    returnMessage = new TextMessage("Added item '" + count + " of " + newItemName.toString().trim() + "' to Shopping List '" + list.name + "'");
+                                                } else {
                                                     list.AddElements(new ShoppingListElement(newItemName.toString().trim(), 1));
                                                     returnMessage = new TextMessage("Added item '1 of " + newItemName.toString().trim() + "' to Shopping List '" + list.name + "'");
                                                 }
-                                            } else if (args[3].equalsIgnoreCase("remove") | args[3].equalsIgnoreCase("r")){
+                                            } else if (args[3].equalsIgnoreCase("remove") | args[3].equalsIgnoreCase("r")) {
                                                 try {
                                                     int listIndex = Integer.parseInt(args[4]);
-                                                    ShoppingListElement ele = list.elements.get(listIndex);
-                                                    list.RemoveElement(listIndex);
-                                                    returnMessage = new TextMessage("Removed item '" + ele.name + "' from Shopping List '" + list.name + "'.");
-                                                }catch (NumberFormatException e){
+                                                    if (listIndex <= list.elements.size()) {
+                                                        ShoppingListElement ele = list.elements.get(listIndex - 1);
+                                                        list.RemoveElement(listIndex - 1);
+                                                        returnMessage = new TextMessage("Removed item '" + ele.name + "' from Shopping List '" + list.name + "'.");
+                                                    } else {
+                                                        returnMessage = new TextMessage(args[4] + " is outside this Shopping List's items' index range!");
+                                                    }
+                                                } catch (NumberFormatException e) {
                                                     returnMessage = new TextMessage(args[4] + " is Not a Number! Specify a Item Index to remove!");
                                                 }
-                                            } else if (args[3].equalsIgnoreCase("cross") | args[3].equalsIgnoreCase("cr")){
+                                            } else if (args[3].equalsIgnoreCase("cross") | args[3].equalsIgnoreCase("cr")) {
                                                 try {
                                                     int listIndex = Integer.parseInt(args[4]);
-                                                    list.elements.get(listIndex).crossed = true;
-                                                    returnMessage = new TextMessage("Crossed item '" + list.elements.get(listIndex).name + "' from Shopping List '" + list.name + "'.");
-                                                }catch (NumberFormatException e){
+                                                    if (listIndex <= list.elements.size()) {
+                                                        list.elements.get(listIndex - 1).crossed = true;
+                                                        returnMessage = new TextMessage("Crossed item '" + list.elements.get(listIndex - 1).name + "' from Shopping List '" + list.name + "'.");
+                                                    } else {
+                                                        returnMessage = new TextMessage(args[4] + " is outside this Shopping List's items' index range!");
+                                                    }
+                                                } catch (NumberFormatException e) {
                                                     returnMessage = new TextMessage(args[4] + " is Not a Number! Specify a Item Index to cross!");
                                                 }
-                                            } else if (args[3].equalsIgnoreCase("change") | args[3].equalsIgnoreCase("ch")){
+                                            } else if (args[3].equalsIgnoreCase("change") | args[3].equalsIgnoreCase("ch")) {
 
                                             }
-                                        }else{
+                                        } else {
                                             returnMessage = new TextMessage("Specify a name/index!");
                                         }
-                                    }else{
+                                    } else {
                                         returnMessage = new TextMessage("Specify a edit command! (add(a)/remove(rm)/cross(cr)/change(ch))");
                                     }
-                                }else{
+                                } else {
                                     returnMessage = new TextMessage(args[2] + " is outside the Shopping List's index range!");
                                 }
-                            }catch (NumberFormatException e){
+                            } catch (NumberFormatException e) {
                                 returnMessage = new TextMessage(args[2] + " is Not a Number!");
                             }
-                        }else{
+                        } else {
                             returnMessage = new TextMessage("Specify a Shopping List to edit!");
                         }
                     }
-                }else{
+                } else {
                     returnMessage = new TextMessage("Specify a subcommand!");
                 }
             }
